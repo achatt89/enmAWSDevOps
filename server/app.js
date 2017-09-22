2522363144786f6ece0c46dcc329dcac9d5ee46a
echeabt <abhijit.chatterjee@ericsson.com>
2017-09-22 16:19:11 +0100

    WIP: Socket Connection

--- server/app.js
+++ server/app.js
@@ -10,17 +10,24 @@ import http from 'http';
 
 
 // Setup server
-var app = express();
-var server = http.createServer(app);
-var socketio = require('socket.io')(server, {
+let app = express();
+let server = http.createServer(app);
+let socketIO = require('socket.io')(server, {
   serverClient: config.env !== 'production',
   path: '/socket.io-client'
 });
 
-require('./config/socketio').default(socketio);
+require('./config/socketio').default(socketIO);
 require('./config/express').default(app);
 require('./routes').default(app);
 
+socketIO.on('connection', function(client) {
+  console.log('Client Connected');
+
+  client.on('join', function(data) {
+    console.log('Client Message: ', data);
+  });
+});
 // Start server
 function startServer() {
   app.angularFullstack = server.listen(config.port, config.ip, function () {
